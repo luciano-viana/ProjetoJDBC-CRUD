52551923006ecceebfb8dc7e6b9f0145ec8a40c8
luciano-viana <lucianoviana12@gmail.com>
2024-07-15 21:15:54 -0300

    Adicionado Driver sqljdbc42 de conexão com SQL Server no projeto

--- ProjetoJDBC-CRUD/src/main/java/conexaojdbc/ConexaoBD.java
+++ ProjetoJDBC-CRUD/src/main/java/conexaojdbc/ConexaoBD.java
@@ -2,13 +2,16 @@ package conexaojdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConexaoBD {
 
-	private static String url = "jdbc:postgresql://localhost:5433/posjava";
+	//private static String url = "jdbc:postgresql://localhost:5433/posjava";
+	private static String url = "jdbc:sqlserver://locahost:1433;databaseName=Automacao;user:sa;password=accaixarapido";//String de conexão do Java com o SQL Server
 	private static String user = "postgres";
 	private static String password = "admin";
 	private static Connection connection = null;
+	
 
 	static {
 		conectar();
@@ -22,7 +25,8 @@ public class ConexaoBD {
 		try {
 
 			if (connection == null) {
-				Class.forName("org.postgresql.Driver");
+				//Class.forName("org.postgresql.Driver");
+				Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 				connection = DriverManager.getConnection(url, user, password);
 				connection.setAutoCommit(false);
 				System.out.println("Conectou com Sucesso!!");
